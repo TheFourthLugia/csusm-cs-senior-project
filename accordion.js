1d79d43587d48ae7eca32b77a8686dc89394a3ce
TheFourthLugia <luong029@csusm.edu>
2024-03-06 21:58:45 -0800

    Added FAQ accordion

---
+++ accordion.js
@@ -0,0 +1,18 @@
+var acc = document.getElementsByClassName("accordion-title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.parentElement.classList.toggle("is-active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
